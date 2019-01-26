b079508cd68eeab01415336629614b8a6e389542
hasanein <hasaninqh@yahoo.com>
2019-01-26 17:56:53 +0000

    Updates

---
+++ precache-manifest.e31a68395ed5b3302386814307a50d2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "34b09b4637b951ae01f3",
+    "url": "/local-welcome-deployment/static/js/runtime~main.34b09b46.js"
+  },
+  {
+    "revision": "57fa1c3a5b4f36f673b2",
+    "url": "/local-welcome-deployment/static/js/main.57fa1c3a.chunk.js"
+  },
+  {
+    "revision": "3bb49a1c7b707a807b8e",
+    "url": "/local-welcome-deployment/static/js/1.3bb49a1c.chunk.js"
+  },
+  {
+    "revision": "57fa1c3a5b4f36f673b2",
+    "url": "/local-welcome-deployment/static/css/main.66e82835.chunk.css"
+  },
+  {
+    "revision": "3bb49a1c7b707a807b8e",
+    "url": "/local-welcome-deployment/static/css/1.83016bab.chunk.css"
+  },
+  {
+    "revision": "2eaf2b36499dbcccc7df245a6935e345",
+    "url": "/local-welcome-deployment/index.html"
+  }
+];
