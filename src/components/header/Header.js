ce16c2b8d4bf455929bdbe454fa3328459e64d26
hasanein <hasaninqh@yahoo.com>
2019-01-26 18:03:48 +0000

    fix logo image path

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Header.css"
 const Header = () => (
   <header className="bg-color-header">
-   <img src="/images/local-welcome.png" alt="logo" id="logo"/>
+   <img src="./images/local-welcome.png" alt="logo" id="logo"/>
   </header>
 );
 export default Header;
